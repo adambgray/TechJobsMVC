fc55fa49ee909cfad986817f34cba208f0d4f78e
Adam Gray <adambgray@gmail.com>
2018-02-27 20:25:18 -0500

    Completed all TODOs

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -13,6 +14,58 @@ namespace TechJobs.Controllers
             return View();
         }
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType.Equals("all"))
+            {
+
+                List<Dictionary<string, string>> searchJobs = new List<Dictionary<string, string>>();
+                foreach (Dictionary<string, string> job in JobData.FindAll())
+                {
+                    bool searchBool = false;
+                    foreach (KeyValuePair<string, string> each in job)
+                    {
+                        string aValue = each.Value;
+                        if (aValue.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            searchBool = true;
+                        }
+                    }
+                    if (searchBool == true)
+                    {
+                        searchJobs.Add(job);
+                    }
+                }
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search Jobs";
+                ViewBag.jobs = searchJobs;
+                return View("Index");
+
+            }
+
+            else
+            {
+                List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+                foreach (Dictionary<string, string> row in JobData.FindAll())
+                {
+                    string aValue = row[searchType];
+
+                    if (aValue.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        jobs.Add(row);
+                    }
+                }
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search Jobs";
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+            
+
+        }
+
+        
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
